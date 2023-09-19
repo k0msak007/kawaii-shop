32d7f177082cf1853fea17cacdfbc759dbcb14cd
komsak007 <98792390+k0msak007@users.noreply.github.com>
2023-09-19 22:44:30 +0700

    Add insert product feature

--- modules/products/productsPatterns/updateProduct.go
+++ modules/products/productsPatterns/updateProduct.go
@@ -3,6 +3,7 @@ package productsPatterns
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/k0msak007/kawaii-shop/modules/entities"
@@ -81,15 +82,86 @@ func (b *updateProductBuilder) updateTitleQuery() {
 	}
 }
 
-func (b *updateProductBuilder) updateDescriptionQuery() {}
+func (b *updateProductBuilder) updateDescriptionQuery() {
+	if b.req.Description != "" {
+		b.values = append(b.values, b.req.Description)
+		b.lastStackIndex = len(b.values)
 
-func (b *updateProductBuilder) updatePriceQuery() {}
+		b.queryFields = append(b.queryFields, fmt.Sprintf(`
+			"description" = $%d
+		`, b.lastStackIndex))
+	}
+}
+
+func (b *updateProductBuilder) updatePriceQuery() {
+	if b.req.Price != 0 {
+		b.values = append(b.values, b.req.Price)
+		b.lastStackIndex = len(b.values)
+
+		b.queryFields = append(b.queryFields, fmt.Sprintf(`
+			"price" = $%d
+		`, b.lastStackIndex))
+	}
+}
 
 func (b *updateProductBuilder) updateCategory() error {
+	if b.req.Category == nil {
+		return nil
+	}
+	if b.req.Category.Id == 0 {
+		return nil
+	}
+
+	query := `
+		UPDATE "products_categories" SET "category_id" = $1
+		WHERE "product_id" = $2;
+	`
+
+	if _, err := b.tx.ExecContext(context.Background(), query, b.req.Category.Id, b.req.Id); err != nil {
+		b.tx.Rollback()
+		return fmt.Errorf("update products_categories failed: %v", err)
+	}
 	return nil
 }
 
 func (b *updateProductBuilder) insertImages() error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
+	defer cancel()
+
+	query := `
+		INSERT INTO "images" (
+			"filename",
+			"url",
+			"product_id"
+		) VALUES
+	`
+
+	valueStack := make([]any, 0)
+	var index int
+	for i := range b.req.Image {
+		valueStack = append(valueStack,
+			b.req.Image[i].FileName,
+			b.req.Image[i].Url,
+			b.req.Id,
+		)
+
+		if i != len(b.req.Image)-1 {
+			query += fmt.Sprintf(`($%d, $%d, $%d),`, index+1, index+2, index+3)
+		} else {
+			query += fmt.Sprintf(`($%d, $%d, $%d);`, index+1, index+2, index+3)
+		}
+		index += 3
+	}
+
+	if _, err := b.tx.ExecContext(
+		ctx,
+		query,
+		valueStack...,
+	); err != nil {
+		b.tx.Rollback()
+		fmt.Printf("insert images failed: %v", err)
+	}
+
 	return nil
 }
 
@@ -101,7 +173,14 @@ func (b *updateProductBuilder) deleteOldImages() error {
 	return nil
 }
 
-func (b *updateProductBuilder) closeQuery() {}
+func (b *updateProductBuilder) closeQuery() {
+	b.values = append(b.values, b.req.Id)
+	b.lastStackIndex = len(b.values)
+
+	b.queryFields = append(b.queryFields, fmt.Sprintf(`
+			WHERE "id" = $%d
+		`, b.lastStackIndex))
+}
 
 func (b *updateProductBuilder) updateProduct() error {
 	return nil
